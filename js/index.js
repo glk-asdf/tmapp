4fbd67edbff53aa4e890259be73173bcd5b7c3cc
glk-asdf <895947135@qq.com>
2016-06-03 17:59:15 +0800

    content

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+var mySwiper2 = new Swiper ('.xlx.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    
+    // // 如果需要滚动条
+    // scrollbar: '.swiper-scrollbar',
+  })
+var mySwiper1 = new Swiper ('.bannerg.swiper-container', {
+    // direction: 'vertical',
+    loop: true,
+    
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    
+    // // 如果需要滚动条
+    // scrollbar: '.swiper-scrollbar',
+  })
