2d853e73a2fc3cf49290e5c2517b976993bb1666
glk-asdf <895947135@qq.com>
2016-06-05 18:41:47 +0800

    edfg

--- js/index.js
+++ js/index.js
@@ -27,3 +27,19 @@ var mySwiper1 = new Swiper ('.bannerg.swiper-container', {
     // // 如果需要滚动条
     // scrollbar: '.swiper-scrollbar',
   })
+// touch.on('.zhibo', 'touchstart', function(ev){
+// 	ev.preventDefault();
+// });
+
+// var target = document.querySelector(".zhibo");
+// target.style.webkitTransition = 'all ease 0.2s';
+
+// touch.on(target, 'swiperight', function(ev){
+// 	this.style.webkitTransform = "translate3d(" + 360 + "px,0,0)";
+// 	log("向右滑动.");
+// });
+
+// touch.on(target, 'swipeleft', function(ev){
+// 	log("向左滑动.");
+// 	this.style.webkitTransform = "translate3d(-" + 360 + "px,0,0)";
+// });
